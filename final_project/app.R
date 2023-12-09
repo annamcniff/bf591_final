830604bc874ccef30f084ab3c14090c0fec08c60
Anna Mcniff <amcniff@scc-ge4.scc.bu.edu>
2023-12-09 12:03:53 -0500

    outline

--- final_project/app.R
+++ final_project/app.R
@@ -9,43 +9,55 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+library(shinydashboard)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Sample Information", tabName = "Sample Information", icon = icon("th")),
+    menuItem("Counts Matrix Exploration", tabName = "Counts Matrix Exploration", icon = icon("th")),
+    menuItem("Differential Expression", tabName = "Differential Expression", icon = icon("th")),
+    menuItem("Gene Set Enrichment Analysis", tabName = "Gene Set Enrichment Analysis", icon = icon("th"))
+    )),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "Sample Information",
+              h2("Add summary table")
+      ),
+      tabItem(tabName = "Counts Matrix Exploration",
+              h2("count matrix stuff")
+      ),
+      tabItem(tabName = "Differential Expression",
+              h2("Add Differential Expression")
+      ),
+      tabItem(tabName = "Gene Set Enrichment Analysis",
+              h2("Add Gene Set Enrichment Analysis")
+      )
     )
-)
+),
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
